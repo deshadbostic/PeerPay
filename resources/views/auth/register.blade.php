bf1623feaeeb6fa1dd4da51c432a27938d59535e
nysaanne <103093865+nysaanne@users.noreply.github.com>
2023-11-18 10:19:56 -0400

    Update register.blade.php
    added fields for additional registration data

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,52 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create A Profile') }}
+        </h2><br>
+   
+    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Name*')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Email*')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        </div><br>
+
+        <div>
+            <x-input-label for="number" :value="__('Phone Number')" />
+            <x-text-input id="number" class="block mt-1 w-full" type="text" name="number" :value="old('number')"  autofocus />
+            <x-input-error :messages="$errors->get('number')" class="mt-2" />
+        </div><br>
+
+        <div>
+            <x-input-label for="address" :value="__('Address*')" />
+            <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')"  required autofocus />
+            <x-input-error :messages="$errors->get('address')" class="mt-2" />
+        </div><br>
+
+        <div>
+            <x-input-label for="country" :value="__('Country*')" />
+            <select id="country" name="country" class="block mt-1 w-full" required>
+                <option value="barbados" @if(old('country') === 'barbados') selected @endif>Barbados</option>
+                <option value="trinidad" @if(old('country') === 'trinidad') selected @endif>Trinidad</option>
+            </select>
+            <x-input-error :messages="$errors->get('country')" class="mt-2" />
+        </div><br>
+
+        <div>
+            <x-input-label for="nationalID" :value="__('National ID')" />
+            <input id="nationalID" class="block mt-1 w-full" type="file" name="nationalID" accept="image/*"/>
+            <x-input-error :messages="$errors->get('nationalID')" class="mt-2" />
+        </div><br>
 
         <!-- Password -->
         <div class="mt-4">
@@ -50,3 +82,4 @@
         </div>
     </form>
 </x-guest-layout>
+
