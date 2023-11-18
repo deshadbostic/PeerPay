32b7fe8ded5b04af623978b3ba2df40cc414548e
nysaanne <103093865+nysaanne@users.noreply.github.com>
2023-11-18 11:01:44 -0400

    Update dashboard.blade.php
    dashboard now shows available balance and transfer history

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,42 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                {{ auth()->user()->name}}
+                {{ __("you're logged in!") }}
+<br><br>
+                <h2 class="font-semibold text-xl text-gray-800 ">
+            {{ __('Available Funds: $') }} {{ auth()->user()->balance}}
+            
+        </h2>
+
+        <br><br>
+                <h2 class="font-semibold text-xl text-gray-800 ">
+            {{ __('Transfer History') }} 
+            
+        </h2>    
+
+<ul>
+    @if(auth()->user()->sentTransfers)
+        @foreach(auth()->user()->sentTransfers as $transfer)
+            <li>{{ $transfer->created_at }} - Sent ${{ $transfer->amount }} to {{ $transfer->receiver->name }}</li>
+        @endforeach
+    @else
+        <li>No sent transfers</li>
+    @endif
+
+    @if(auth()->user()->receivedTransfers)
+        @foreach(auth()->user()->receivedTransfers as $transfer)
+            <li>{{ $transfer->created_at }} - Received ${{ $transfer->amount }} from {{ $transfer->sender->name }}</li>
+        @endforeach
+    @else
+        <li>No received transfers</li>
+    @endif
+</ul>
+
+
                 </div>
+
+              
             </div>
         </div>
     </div>
