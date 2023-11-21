468805373889b95123112973f5fa8d1b630d2afa
Zeliapooh <tize_girl@live.com>
2023-11-21 16:50:41 -0400

    Added delete button

--- resources/views/payees/index.blade.php
+++ resources/views/payees/index.blade.php
@@ -6,6 +6,29 @@
     <ul>
         @foreach($payees as $payee)
             <li>{{ $payee->name }} <br>({{ $payee->email }})</li><br>
+            <div>
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path
+                                    d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                            </svg>
+                        </button>
+                    </x-slot>
+                    <x-slot name="content">
+                        <form method="POST" action="{{ route('payee.destroy', $payee) }}">
+                            @csrf
+                            @method('delete')
+                            <x-dropdown-link :href="route('payee.destroy', $payee)"
+                                onclick="event.preventDefault(); this.closest('form').submit();">
+                                {{ __('Delete') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            </div>
         @endforeach
     </ul>
     <br><br>
