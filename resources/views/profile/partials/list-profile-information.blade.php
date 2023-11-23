8f60f1ec4582238fc8c38d6ce47f45182715c4b4
nysaanne <103093865+nysaanne@users.noreply.github.com>
2023-11-22 21:32:25 -0400

    Add files via upload
    listing profile info

---
+++ resources/views/profile/partials/list-profile-information.blade.php
@@ -0,0 +1,25 @@
+<section>
+  <header>
+    <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Profile Information') }}
+        </h2><br>
+</header>
+        <p class="text-md font-medium text-gray-900">
+            {{ __('Name: ' )}} {{ auth()->user()->name}}
+</p>
+        <p class="text-md font-medium text-gray-900">
+            {{ __('Email: ' )}} {{ auth()->user()->email}}
+</p> 
+
+<p class="text-md font-medium text-gray-900">
+            {{ __('Number: ' )}} {{ auth()->user()->number}}
+</p> 
+<p class="text-md font-medium text-gray-900">
+            {{ __('Address: ' )}} {{ auth()->user()->address}}
+</p> 
+<p class="text-md font-medium text-gray-900">
+    {{ __('Country: ') }} {{ ucfirst(auth()->user()->country) }}
+</p>
+
+    
+    </section>
