55c98d8dba1246ad705acf11c686566e733f9f8c
ReinGas <61610476+ReinGas@users.noreply.github.com>
2023-11-24 20:59:03 -0400

    Add files via upload

--- database/migrations/2023_11_29_163951_add_new_fields_users.php
+++ database/migrations/2023_11_29_163951_add_new_fields_users.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('nationalID')->nullable();
             $table->string('address');
             $table->string('country');
+            $table->double('account');
         });
     }
 
@@ -29,6 +30,7 @@ return new class extends Migration
             $table->dropColumn('phone');
             $table->dropColumn('address');
             $table->dropColumn('country');
+            $table->dropColumn('account');
         });
     }
 };
